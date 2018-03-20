1b465f63bd698797a2845523ba43844a0d82f83a
Bibek <jujulalshrestha@gmail.com>
2018-03-20 14:31:41 -0500

    readme updated!

--- DataStructure/ReverseDigits.java
+++ DataStructure/ReverseDigits.java
@@ -1,4 +1,7 @@
 package DataStructure;
+/*
+* Write a program which takes an integer and returns the integer corresponding to the degits of the input written in reverse order. For example 245 is 542, and 45 is 54.
+* */
 public class ReverseDigits {
 
 	public int reverse(int num){
